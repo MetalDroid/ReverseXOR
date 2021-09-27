8363172580b9c49acddd02a34479e89bcd211eab
MetalDroid <MetalDroid@users.noreply.github.com>
2021-09-27 09:37:03 +0200

    add docs + change access

--- src/com/metal/reversexor/ReverseXor.java
+++ src/com/metal/reversexor/ReverseXor.java
@@ -3,39 +3,42 @@ package com.metal.reversexor;
 public class ReverseXor {
 
     /**
-     * @param input Text input to encrypt
-     * @param password Password used in xor
-     * @param reverseLength Maximum size of each group of characters to reverse
-     * @return Encrypted string
+     * Main method of encrypting a string.
+     * @param input Text input to encrypt.
+     * @param password Password used in xor.
+     * @param reverseLength Maximum size of each group of characters to reverse.
+     * @return Encrypted string.
      */
     public static String ReverseXOREnc(String input, String password, int reverseLength){
         return XorString(reverseMod(input, reverseLength), password);
     }
 
     /**
-     * @param input Text input to decrypt
-     * @param password Password used in xor
-     * @param reverseLength Maximum size of each group of characters to reverse
-     * @return Decrypted string
+     * Main method of decrypting a string.
+     * @param input Text input to decrypt.
+     * @param password Password used in xor.
+     * @param reverseLength Maximum size of each group of characters to reverse.
+     * @return Decrypted string.
      */
     public static String ReverseXORDec(String input, String password, int reverseLength){
         return reverseMod(XorString(input, password), reverseLength);
     }
 
     /**
-     * @param inputChar original char
-     * @param passwordChar password char
-     * @return encrypted char
+     * Intermediate method to apply xor to a character.
+     * @param inputChar original char.
+     * @param passwordChar password char.
+     * @return encrypted char.
      */
     private static char Xor(char inputChar, char passwordChar){
         return (char) (inputChar ^ passwordChar);
     }
 
     /**
-     * Simple xor encryption
-     * @param input Priginal string
-     * @param password Password string
-     * @return Encrypted string
+     * Intermediate method to apply xor to a String.
+     * @param input Original string.
+     * @param password Password string.
+     * @return Encrypted string.
      */
     public static String XorString (String input, String password){
         StringBuilder res = new StringBuilder(input);
@@ -46,9 +49,11 @@ public class ReverseXor {
     }
 
     /**
+     * Inverts a string with a specified block size.
+     * Example using reverseLength 4: Hello World -> lleHoW odlr
      * @param input Text input
      * @param reverseLength Maximum size of each group of characters to reverse
-     * @return Reversed (mod) text: Hello World -> Hello World -> lleHow odlr
+     * @return Reversed (mod) text.
      */
     public static String reverseMod(String input, int reverseLength){
         StringBuilder res = new StringBuilder();
